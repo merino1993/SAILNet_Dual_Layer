659d2af45265dde1d95105d5ee95b845207a5cbc
merino1993 <merino.christian@berkeley.edu>
2015-07-16 15:38:40 -0700

    combined plots to one pdf page and found five strongest connections from Layer 2 to Layer 1 Neurons

--- plotting.py
+++ plotting.py
@@ -21,7 +21,23 @@ OC1=Q1.shape[1]/N
 M1=Q1.shape[1]
 OC2=Q2.shape[1]/M1
 
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
+pp = PdfPages('plots.pdf')
+
+indxs=np.zeros((5,5))
+for n in range(5):
+    v=Q2[:,n]
+    for c in range(5):
+        idx=np.argmax(abs(v))
+        indxs[n,c]=idx
+        v[idx]=0
+
+plt.figure()
+plt.plot(indxs)
+plt.title("Strongest Layer 2 Neuron Connections to Layer 1 Neurons")
+pp.savefig()
+#Find the 2 layer neurons that have the strongest connection to the 1 layer neurons
 
 K = Q2 #Q2.shape = (25,25)
 idx = K.argsort() #sorts in increasing order
@@ -32,35 +48,33 @@ print K[idx[-3]], ' index '+str(idx[-3])
 print K[idx[-4]], ' index '+str(idx[-4])
 print K[idx[-5]], ' index '+str(idx[-5])
 
-plot1=plt.figure()
+
+plt.figure()
 plt.plot(reconstruction_error1)
 plt.title("Mean Squared Error of SAILNet's Reconstruction with Layer 1")   
-plt.savefig('plot1.png')
+pp.savefig()
 
-plot2=plt.figure()
 plt.figure()
 plt.plot(reconstruction_error2)
 plt.title("Mean Squared Error of SAILNet's Reconstruction with Layer 2")
-plt.savefig('plot2.png')
+pp.savefig()
 
-plot3=plt.figure()
 plt.figure()
 plt.plot(SNR_1)
 plt.title("Signal to Noise ratio for Layer 1")
-plt.savefig('plot3.png')
+pp.savefig()
 
-plot4=plt.figure()
 plt.figure()
 plt.plot(SNR_2)
 plt.title("Signal to Noise ratio for Layer 2")
-plt.savefig('plot4.png')
+pp.savefig()
 
 #plot all receptive fields
-plot5=plt.figure()
 plt.figure()
 side = int(np.sqrt(N))
 img = tile_raster_images(Q1.T, img_shape = (side,side), tile_shape = (2*side,side*OC1/2), tile_spacing=(1, 1), scale_rows_to_unit_interval=True, output_pixel_vals=True)
 plt.imshow(img,cmap=plt.cm.Greys, interpolation='nearest')
 plt.title('Receptive Fields')
-plt.savefig('plot5.png')
+pp.savefig()
+pp.close()
 plt.show()
